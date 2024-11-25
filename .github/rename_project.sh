ec0a1b7f0040124b04a3f7b8764b35c3c507d501
eveyuyi <eveyuyi@users.noreply.github.com>
2024-11-25 08:54:02 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="eveyuyi"
+original_name="aigc_detection"
+original_urlname="AIGC_detection"
+original_description="Awesome aigc_detection created by eveyuyi"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv aigc_detection $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ aigc_detection/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for aigc_detection."""
+
+from aigc_detection.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
